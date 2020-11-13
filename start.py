8878c6ee806a0468e830b71bac5dcd18b50c6211
Sayid <kongtao01@beyondsoft.com>
2020-11-13 11:01:08 +0800

    add android

--- start.py
+++ start.py
@@ -2,19 +2,21 @@ from appium import webdriver
 import time
 import os
 
-desired_caps_ios_wechat = {
-    "platformName": "iOS",
-    "PlatformVersion": os.getenv('APP_DEVICE_VERSION', "12.2"),
-    "deviceName": os.getenv('APP_DEVICE_NAME', "iPhone"),
-    "automationName": "XCUITest",
-    "udid": os.getenv("APP_UDID", "029d553ea04ba899509dc0630fda19bdac61231a"),
-    "bundleId": os.getenv("APP_BUNDLEIDENTIFIER", "com.tencent.xin"),
+desired_caps_android_wechat = {
+    "platformName": "Android",
+    "platformVersion": os.getenv("APPIUM_DEVICE_VERSION", "10"),
+    "automationName": os.getenv("APPIUM_AUTOMATION_NAME", "UiAutomator2"),
+    "appActivity": os.getenv("APPIUM_APP_ACTIVITY", "com.tencent.mm.ui.LauncherUI"),
+    "appPackage": os.getenv("APPIUM_APP_PACKAGE", "com.tencent.mm"),
+    "deviceName": os.getenv("APPIUM_DEVICE_NAME", "AKC7N18907000186"),
     "newCommandTimeout": 7200,
-    "startIWDP": True,
-    "webDriverAgentUrl": os.getenv("WEBDRIVERAGENT_URL", "http://localhost:8100")
+    "noReset": True,
+    "unicodeKeyboard": True,
+    'resetKeyboard': True,
+    'chromeOptions': {'androidProcess': 'com.tencent.mm:tools'}
 }
 
-driver = webdriver.Remote(os.getenv("APPIUM_URL", 'http://localhost:4723/wd/hub'), desired_caps_ios_wechat)
+driver = webdriver.Remote(os.getenv("APPIUM_URL", 'http://localhost:4723/wd/hub'), desired_caps_android_wechat)
 
 print("Success to init driver")
 time.sleep(15)
